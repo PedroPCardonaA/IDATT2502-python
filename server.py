bb96ae1c63b83fb2bc6497ad2d25db90c945cd84
Pedro Cardona <pedropca@stud.ntnu.no>
2023-10-31 18:44:46 +0100

    Endpoint was updated  to get category and save the image

--- server.py
+++ server.py
@@ -7,16 +7,17 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms
 from pathlib import Path
+import random
 app = Flask(__name__)
 
 # Sample data (in-memory storage)
-model = models.garbage_classifier_5L_attention(input_shape=3, hidden_units=64, output_shape=8)
-model.load_state_dict(torch.load("models/model5l.pth",map_location=torch.device('cpu')))
+model = models.garbage_classifier_5L_attention(input_shape=3, hidden_units=64, output_shape=6)
+model.load_state_dict(torch.load("models/model(4).pth",map_location=torch.device('cpu')))
 preprocess = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
-classes = ['cardboard', 'food', 'glass', 'hazardous','metal', 'paper', 'plastic', 'trash']
+classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 
 # PUT endpoint
 @app.route('/data/<key>', methods=['PUT'])
@@ -36,6 +37,10 @@ def post_image():
         if 'file' not in request.files:
             return "No file part in the request", 400
 
+        category = request.form['category']
+        print(f"category {category}")
+
+
         file = request.files['file']
         if file.filename == '':
             return "No selected file", 400
@@ -44,8 +49,20 @@ def post_image():
         if file.content_type.split('/')[0] != 'image':
             return "File is not an image", 400
 
+
+        folder_path = Path.cwd() / "photos" / "train" / "trash"
+        
+        #save image in forlder 
+        if random.random() < 0.8:
+            folder_path = Path.cwd() / "photos" / category
+        else:
+            folder_path = Path.cwd() / "photos" / category
+
+        #file.save(folder_path / str(random.randint(0,10000000000000).__str__()+".jpg"))
+
         # Read the image data into a PIL Image
         image = Image.open(io.BytesIO(file.read()))
+        image.save(folder_path / str(random.randint(0,10000000000000).__str__()+".jpg"))
 
         # Convert the PIL Image to a NumPy array
         image_array = np.array(image)
